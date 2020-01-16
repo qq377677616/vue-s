dda4bb17ebe87c7087c2b512cac95101bf4366aa
songziquan <377677616@qq.com>
2020-01-16 15:57:29 +0800

    szq20200116

--- src/api/config.js
+++ src/api/config.js
@@ -36,7 +36,8 @@ const SECRETKEY = {
 //项目开关配置信息
 const PROJECT_CONFIG = {
   wx_jssdk_type: 1,//获取微信jssdk注册参数类型：0为加载script标签获取，1为请求接口形式获取
-  getUserInfo: {//从后端链接授权后获取后台带过来的用户数据
+  /*从后端链接授权后获取后台带过来的用户数据配置*/
+  getUserInfo: {
     is_open: true,//是否加载页面后自动获取后存储到本地两种缓存中
     type: 1,//获取方式：1为从本地缓存或者url中获取，2为请求后端接口获取
     get_data_list: ['openid', 'token', 'nickname', 'avatar'],//从本地缓存或者url中获取的字段名列表
@@ -44,13 +45,15 @@ const PROJECT_CONFIG = {
   },
   is_loading_page: true,//是否配置loading预加载页
   is_page_locking: true,//是否锁定页面上拉下拉滑动
-  is_wx_share: true,//是否配置分享  
-  is_background_music: {//背景音乐配置
+  is_wx_share: true,//是否配置分享
+  /*背景音乐配置*/  
+  is_background_music: {
     is_open: false,//是否开启背景音乐
     type: 0,//播放形式：0为audio标签播放，1为当页面中有video标签时安卓机采用js方法播放，2为采用js方法播放
     constrols_btn_url: [require('assets/images/music_01.png'), require('assets/images/music_02.png')]//播放暂停按钮icon图片路径 
   },
-  vConsole: {//绿标配置
+  /*绿标配置*/
+  vConsole: {
     is_open: 1,//是否开启绿标:0为不开启，1为永久开启，2为本地开发不开启线上永久开启，3为本地开发不开启线上在项目上线日期前开启
     green_label_color: "linear-gradient(-90deg, #F28F29 0, #027C60 100%)",//绿色浮标颜色
     green_label_title: "未上线版本"//绿色浮标显示文字
